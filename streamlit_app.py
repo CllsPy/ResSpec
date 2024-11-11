85cf55172d402ad01540fa3b081b4a5bec502fa9
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-11 22:42:35 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if img is not None:
         percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
         answer = {labels[index[0]], percentage[index[0]].item()}
         sts = (percentage[index[0]].item())
-        val = st.slider('Percentage',min_value=0.0, max_value=sts)  
+        val = st.slider('Percentage',min_value=sts, max_value=100)  
         st.code(val, language="markdown")
 
 
