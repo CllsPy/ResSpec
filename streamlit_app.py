9c0a2d496a5350b8fae7da763bf4e0e5ffbe2def
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-11 22:10:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ preprocess = transforms.Compose([
 img = st.file_uploader(
     "Choose a CSV file", accept_multiple_files=True
 )
-img = st.image(img)
+img = img.covert()
 if img is not None:
         img_t = preprocess(img)
         batch_t = torch.unsqueeze(img_t, 0)
