53feb26a563862e87d5fe0e5fdf165284e3dfe57
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-11 21:41:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,17 @@ import streamlit as st
 from torchvision import models
 st.markdown(dir(models))
 
+resnet = models.resnet101(pretrained=True)
+
+preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )])
+
+from PIL import Image
+img = Image.open("/content/drive/MyDrive/Colab Notebooks/season_03/dlwp/data/snake.jpg")
+img
