971bb302e3c998192f8639e6e93450b9d56a041b
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-12 03:59:16 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,10 @@ with st.sidebar:
 # cols
 col1, col2, col3 = st.columns(3)
 
-with st.container(height=300):
-        st.markdown("## 1. Upload an imagem")
-        img = st.file_uploader("", type=["jpg", "png", "jpeg"])
+with col1:
+        with st.container(height=300):
+                st.markdown("## 1. Upload an imagem")
+                img = st.file_uploader("", type=["jpg", "png", "jpeg"])
 
 if img is not None:
         img = Image.open(img)
