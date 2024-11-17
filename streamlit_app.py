4ec0f1c82fcb15e3c083e6b48b8317d4fdaa4904
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-17 18:55:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ with st.form('Image Classifier'):
 
         # st.markdown("## 3. Label for your image")
         # plt.grid(True)
-        st.pyplot(df)
+        st.pyplot(fig)
         #df = df['Probability'].max(axis=0)
         #df
         #df = df.loc[df['Probability'].idxmax()]
