2cd15b93d0a7d899e252caa6385289b72655e027
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-17 22:04:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ with st.sidebar:
         radio = st.checkbox('Which Species I can classify?')
 
         if radio:
-                st.markdown([Classes](https://deeplearning.cms.waikato.ac.nz/user-guide/class-maps/IMAGENET/))
+                st.markdown('[Classes](https://deeplearning.cms.waikato.ac.nz/user-guide/class-maps/IMAGENET/)')
                 
 with st.form('Image Classifier'):
         st.markdown("## 1. Upload an imagem")
