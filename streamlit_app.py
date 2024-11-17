f06f04bc939b9bcb640e9eaf0bb1c41339aa08ca
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-17 18:02:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,13 @@ with st.sidebar:
 
 with st.form('Image Classifier'):
         st.markdown("## 1. Upload an imagem")
-        st.info('''
-                                Please upload an image of an animal (in JPG, PNG, or JPEG format) 
-                                for classification.  The model will analyze the image and predict the most likely animal species.''')
+        st.info(
+                """Please upload an image of an animal 
+                (in JPG, PNG, or JPEG format)  for classification.  
+                The model will analyze the image and predict the most likely animal species.'""")
+        
         img = st.file_uploader("", type=["jpg", "png", "jpeg"])
+        st.form_submit_button('Submit Image")
 
 if img is not None:
         img = Image.open(img)
