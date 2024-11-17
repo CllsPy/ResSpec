8e56a00319a1ec827e605a23d3cdc607e871c78e
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-17 21:19:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,8 @@ with st.form('Image Classifier'):
                 st.markdown("## 3. Label for your image")
                 plt.grid(True)
                 st.pyplot(fig)
-                st.info(zip(top_labels, top_percentages))
+                answer = zip(top_labels, top_percentages)
+                st.info(answer)
 
         except Exception as inst:
                 st.info("Oops!  That was no valid **IMAGE**.  Try again...")
